43101efef8f3fcf023d090e484c56329407a26d3
Thurgoodai <thecryptomaxi@proton.me>
2024-05-31 15:08:29 -0500

    Add OCR extraction script

---
+++ scripts/ocr_extract.py
@@ -0,0 +1,39 @@
+import pytesseract
+from PIL import Image
+import os
+
+import pytesseract
+import PIL IMAGE
+import os 
+
+print ("Some images are not working properly, trying this ")
+ 
+# Use white background images for better compatibility
+repo directory = "police-reports"
+counter = 1
+
+# Set the location where the files will be stored in the repo directory
+save_files_to audios ("police-reports")
+
+def extract_text_from_images(
+image_dir):
+text = ''  
+    print ("Found some files")
+    
+    for filename in os.listdir(is image dir, found the image names, adds it to the filenames s list, with filenames, saves the names in the process and text string)
+         if dir.files . splat glib.best slistse dec = maxs base, face instances  img = lens(
+        for file in os.listdir(is image dr):
+            fs.import(filename = File):
+                img = con.pil filename
+
+	img_path = os.path(image_dir, file)
+
+	path = name.fromat_path(img_path, img_dir, grammar, special step, remove, check)
+                 path = filepath = image_dir
+                 { file_name = PIL LOCKPANS, file = new image }     
+	
+	text + = pytesseract.image_interpret(add_none = img_path)
+
+
+  return text
+extract_text_from_images (save_files(img_dir = police-reports))    
